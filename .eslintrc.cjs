8ed61b5a105d64939929164530b6ddff6e87af1c
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-04-24 18:08:55 +0400

    Initial Commit (updated)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   extends: '@it-incubator/eslint-config',
-  overrides: [
-    {
-      files: ['**/*.stories.tsx'],
-      rules: {
-        'react-hooks/rules-of-hooks': 'off',
-        'no-console': 'off',
-      },
-    },
-  ],
+  // overrides: [
+  //   {
+  //     files: ['**/*.stories.tsx'],
+  //     rules: {
+  //       'react-hooks/rules-of-hooks': 'off',
+  //       'no-console': 'off',
+  //     },
+  //   },
+  // ],
 }
