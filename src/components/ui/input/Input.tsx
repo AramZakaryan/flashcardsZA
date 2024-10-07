5dacb5e9c76f39ebbb7b26a2f8982c448a6029a1
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-10-08 01:47:38 +0400

    fix Input: add forwardRef

--- src/components/ui/input/Input.tsx
+++ src/components/ui/input/Input.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ComponentPropsWithoutRef, ReactElement, useState } from 'react'
+import { ChangeEvent, ComponentPropsWithoutRef, forwardRef, ReactElement, useState } from 'react'
 import { Close, EyeOffOutline, EyeOutline, Search } from '../icons'
 import s from './input.module.scss'
 import { Body2span, Caption } from '@/components/ui/typography'
@@ -14,82 +14,89 @@ export type InputProps = {
   errorMessage?: string
 } & ComponentPropsWithoutRef<'input'>
 
-export const Input = ({
-  className,
-  error,
-  errorMessage,
-  label,
-  onChange,
-  onValueChange,
-  placeholder,
-  variant,
-  disabled,
-  initialValue = '',
-  ...rest
-}: InputProps) => {
-  const [showPassword, setShowPassword] = useState(false)
-  const [currentValue, setCurrentValue] = useState(initialValue)
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className,
+      error,
+      errorMessage,
+      label,
+      onChange,
+      onValueChange,
+      placeholder,
+      variant = 'text',
+      disabled,
+      initialValue = '',
+      ...restProps
+    },
+    forwardedRef
+  ) => {
+    const [showPassword, setShowPassword] = useState(false)
+    const [currentValue, setCurrentValue] = useState(initialValue)
 
-  let iconLeft: Nullable<ReactElement> = null
-  let iconRight: Nullable<ReactElement> = null
-  let finalType
-  switch (variant) {
-    case 'password':
-      const handleEyeClick = () => {
-        !disabled && setShowPassword((prevState) => !prevState)
-      }
-      iconRight = showPassword ? (
-        <EyeOffOutline onClick={handleEyeClick} />
-      ) : (
-        <EyeOutline onClick={handleEyeClick} />
-      )
-      finalType = showPassword ? 'text' : 'password'
-      break
-    case 'search':
-      iconLeft = <Search />
-      iconRight = currentValue ? <Close onClick={() => setCurrentValue('')} /> : null
-      finalType = 'search'
-      break
-    case 'text':
-      finalType = 'text'
-      break
-    default:
-      iconRight = null
-  }
+    let iconLeft: Nullable<ReactElement> = null
+    let iconRight: Nullable<ReactElement> = null
+    let finalType
+    switch (variant) {
+      case 'password':
+        const handleEyeClick = () => {
+          !disabled && setShowPassword((prevState) => !prevState)
+        }
+        iconRight = showPassword ? (
+          <EyeOffOutline onClick={handleEyeClick} />
+        ) : (
+          <EyeOutline onClick={handleEyeClick} />
+        )
+        finalType = showPassword ? 'text' : 'password'
+        break
+      case 'search':
+        iconLeft = <Search />
+        iconRight = currentValue ? <Close onClick={() => setCurrentValue('')} /> : null
+        finalType = 'search'
+        break
+      case 'text':
+        finalType = 'text'
+        break
+      default:
+        iconRight = null
+    }
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    setCurrentValue(e.target.value)
-    onChange?.(e)
-    onValueChange?.(e.currentTarget.value)
-  }
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+      setCurrentValue(e.target.value)
+      onChange?.(e)
+      onValueChange?.(e.currentTarget.value)
+    }
 
-  return (
-    <div className={clsx(className, s.rootContainer)}>
-      {label && (
-        <label className={clsx(s.label, disabled && s.disabled)}>
-          <Body2span>{label}</Body2span>
-        </label>
-      )}
-      <div className={s.fieldContainer}>
-        {!!iconLeft && <div className={clsx(s.iconLeft, disabled && s.disabled)}>{iconLeft}</div>}
-        <input
-          className={clsx(s.input, error && s.error, variant === 'search' && s.search)}
-          onChange={handleChange}
-          placeholder={placeholder}
-          type={finalType}
-          disabled={disabled}
-          value={currentValue}
-          {...rest}
-        />
-        {!!iconRight && (
-          <div className={clsx(s.iconRight, disabled && s.disabled)}>{iconRight}</div>
+    return (
+      <div className={clsx(className, s.rootContainer)}>
+        {label && (
+          <label className={clsx(s.label, disabled && s.disabled)}>
+            <Body2span>{label}</Body2span>
+          </label>
+        )}
+        <div className={s.fieldContainer}>
+          {!!iconLeft && <div className={clsx(s.iconLeft, disabled && s.disabled)}>{iconLeft}</div>}
+          <input
+            className={clsx(s.input, error && s.error, variant === 'search' && s.search)}
+            onChange={handleChange}
+            placeholder={placeholder}
+            type={finalType}
+            disabled={disabled}
+            value={currentValue}
+            ref={forwardedRef}
+            {...restProps}
+          />
+          {!!iconRight && (
+            <div className={clsx(s.iconRight, disabled && s.disabled)}>{iconRight}</div>
+          )}
+        </div>
+        {error && (
+          <div className={s.error}>
+            <Caption>{errorMessage}</Caption>
+          </div>
         )}
       </div>
-      {error && (
-        <div className={s.error}>
-          <Caption>{errorMessage}</Caption>
-        </div>
-      )}
-    </div>
-  )
-}
+    )
+  }
+)
+Input.displayName = 'Input'
