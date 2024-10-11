b15329b3c68025df222e64eea12fd704eff57b1a
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-10-11 19:36:49 +0400

    add CheckEmail

---
+++ src/components/forms/checkEmail/CheckEmail.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { CheckEmail } from './CheckEmail'
+
+const meta = {
+  title: 'Auth/CheckEmail',
+  component: CheckEmail,
+  tags: ['autodocs'],
+} satisfies Meta<typeof CheckEmail>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  argTypes: {
+    onSubmit: {
+      action: 'on Submit',
+    },
+  },
+  args: {
+    email: 'example@mail.com',
+  },
+}
