6f617aebcbb6f792680781867daad80afaa631e6
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-10-16 17:09:40 +0400

    chore: fix Tabs storybooks

--- src/components/ui/tabs/Tabs.stories.tsx
+++ src/components/ui/tabs/Tabs.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta } from '@storybook/react'
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs/Tabs'
+import { Tabs } from '@/components/ui/tabs/Tabs'
 
 export default {
   title: 'Components/Tabs',
@@ -7,32 +7,27 @@ export default {
 } as Meta<typeof Tabs>
 
 export const TabsDefault = () => (
-  <Tabs style={{ width: '400px' }}>
-    <TabsList>
-      <TabsTrigger value="tab1">Switcher</TabsTrigger>
-      <TabsTrigger value="tab2">Switcher</TabsTrigger>
-      <TabsTrigger value="tab3">Switcher</TabsTrigger>
-      <TabsTrigger value="tab4">Switcher</TabsTrigger>
-    </TabsList>
-    {/*<TabsContent value="account">Make changes to your account here.</TabsContent>*/}
-    {/*<TabsContent value="password">Change your password here.</TabsContent>*/}
-    {/*<TabsContent value="info">Change your info here.</TabsContent>*/}
-  </Tabs>
+  <Tabs
+    label={'Tabs'}
+    defaultValue={'val2'}
+    options={[
+      { value: 'val1', text: 'Switcher 1' },
+      { value: 'val2', text: 'Switcher 2' },
+      { value: 'val3', text: 'Switcher 3' },
+      { value: 'val4', text: 'Switcher 4' },
+    ]}
+  />
 )
 
-export const TabsWithDisabledTab = () => (
-  <Tabs style={{ width: '400px' }}>
-    <TabsList>
-      <TabsTrigger value="tab1">Switcher</TabsTrigger>
-      <TabsTrigger disabled value="tab2">
-        Switcher
-      </TabsTrigger>
-      <TabsTrigger value="tab3">Switcher</TabsTrigger>
-      <TabsTrigger value="tab4">Switcher</TabsTrigger>
-    </TabsList>
-    {/*<TabsContent value="tab1">Make changes here.</TabsContent>*/}
-    {/*<TabsContent value="tab2">Make changes here.</TabsContent>*/}
-    {/*<TabsContent value="tab3">Make changes here.</TabsContent>*/}
-    {/*<TabsContent value="tab4">Make changes here.</TabsContent>*/}
-  </Tabs>
+export const TabsWithDisabled = () => (
+  <Tabs
+    label={'Tabs'}
+    defaultValue={'val2'}
+    options={[
+      { value: 'val1', text: 'Switcher 1' },
+      { value: 'val2', text: 'Switcher 2' },
+      { value: 'val3', text: 'Switcher 3', disabled: true },
+      { value: 'val4', text: 'Switcher 4' },
+    ]}
+  />
 )
