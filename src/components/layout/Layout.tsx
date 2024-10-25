e99a0d0b4a3f6011761e91fe2b28c3a8766738c3
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-10-25 14:33:18 +0400

    add: Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,16 +3,28 @@ import { ComponentPropsWithoutRef } from 'react'
 import { Header } from '@/components'
 import { clsx } from 'clsx'
 import { Outlet } from 'react-router-dom'
+import { useMeQuery } from '@/services/auth'
 
 export type LayoutProps = ComponentPropsWithoutRef<'div'>
 
-// ZA: this components will be changes according to the Header change
+// type AuthContext = {
+//   isAuth: boolean
+// }
+
+// ZA: this components may be changed according to the Header changes
 export const Layout = ({ children, className, ...restProps }: LayoutProps) => {
+  const { data, isError, isLoading } = useMeQuery(undefined, { skip: true })
+  const isAuth = !isError && isLoading
+
+  const { avatar, name, email } = data || {}
+
+  if (isLoading) return <h4>loading...</h4>
+
   return (
     <div className={clsx(s.layout, className)} {...restProps}>
-      <Header />
+      <Header avatar={avatar} name={name} email={email} />
       <div className={s.layoutContent}>
-        <Outlet />
+        <Outlet context={{ isAuth }} />
         {children}
       </div>
     </div>
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,20 +3,22 @@ import { ComponentPropsWithoutRef } from 'react'
 import { clsx } from 'clsx'
 import { H4span } from '@/components'
 import UserDropdown from '@/components/ui/dropdown/UserDropdown'
-import johnImage from '@/assets/image/john.png'
+import { User } from '@/services/auth'
 
-export type HeaderProps = ComponentPropsWithoutRef<'header'>
+export type HeaderProps = Partial<Pick<User, 'avatar' | 'name' | 'email'>> &
+  ComponentPropsWithoutRef<'header'>
 
-export const Header = ({ className, children, ...restProps }: HeaderProps) => {
+export const Header = ({ avatar, name, email, className, children, ...restProps }: HeaderProps) => {
   return (
     <header className={clsx(s.header, className)} {...restProps}>
       <div className={s.headerContent}>
         <H4span>FleshCards</H4span>
+        <H4span>{name}</H4span> {/* ZA: temporary, forDel */}
         {children}
         <UserDropdown
-          avatar={johnImage}
-          name={'John'}
-          email={'john@example.com'}
+          avatar={avatar}
+          name={name}
+          email={email}
           onSignOut={() => {
             console.log('signOut')
           }}
